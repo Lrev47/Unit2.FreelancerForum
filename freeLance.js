85335afedffc25390e5456860ad4db9fc1630201
Lrev47 <luis.revilla47@yahoo.com>
2023-11-22 18:44:39 -0600

    added work prompt

---
+++ freeLance.js
@@ -0,0 +1,15 @@
+
+const freelancers = [
+    { name: "Gary", price: 25, occupation: "gardener" },
+    { name: "Mr. Krabs", price: 51, occupation: "Resturant Owner" },
+    { name: "Pearl", price: 43, occupation: "Cheerleader" },
+    { name: "Squidward", price: 81, occupation: "Artist" },
+    { name: "smitty werbenjagermanjensen", price: 43, occupation: "Hes number 1" },
+    { name: "hash slinging slasher", price: 76, occupation: "programmer" },
+    { name: "Doodle Bob", price: 47, occupation: "teacher" },
+    { name: "Kevin", price: 72, occupation: "driver" },
+];
+
+const p = document.querySelector("div")
+p.textContent = "Hello World"
+
